bad80995c904bb5e38bd73dc00b1b6ae4b40766c
matheusapostulo <matheusapostulo10@gmail.com>
2022-12-15 20:22:22 -0300

    Par de barreiras definidos

--- js/flappy.js
+++ js/flappy.js
@@ -16,7 +16,38 @@ function Barreira(reverso = false){
     this.setAltura = altura => corpo.style.height = `${altura}px`
 }
 
+function parDeBarreiras(altura, abertura, x){
+    // cria uma div para o par de barreiras
+    this.elemento = novoElemento('div', 'par-barreiras')
 
+    // cria objeto barreira superior
+    this.superior = new Barreira(true)
+    // cria objeto barreira inferior 
+    this.inferior = new Barreira(false)
+
+    // faz o appendChild das duas barreiras
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    // cria função pra sortear a abertura
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura-abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+
+}
+
+const b = new parDeBarreiras(600,200,400)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
 
 /* const barreira_teste = new Barreira(true)
 barreira_teste.setAltura(200)
